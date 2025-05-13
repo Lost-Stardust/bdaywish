1549184cc492c1e0dd0afd7ed77816b8c4d4b098
Isa <isaismail3000@gmail.com>
2025-05-13 15:03:26 +0300

    Import images and set img tag src

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+import ann from "./images/ann.png";
+import giftBox from "./images/gift-box.png";
+
+// Set src of img tags
+const heart = document.querySelector(".heart");
+heart.src = ann;
+const gift = document.querySelector(".gift-box");
+gift.src = giftBox;
