33d2224db3851d9dbec799fb4bc25e1c28be4328
Isa <isaismail3000@gmail.com>
2025-05-15 18:54:20 +0300

    Add audio, rework on click events
    add new audio and set volume, play it on click.
    Remove resetHeart function and set styles to manipulate
    heart in listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import ann from "./images/ann.png";
 import giftBox from "./images/gift-box.png";
+import pop from "./images/pop.mp3";
 
 // Set src of img tags
 const heart = document.querySelector(".heart");
@@ -8,13 +9,19 @@ heart.src = ann;
 const gift = document.querySelector(".gift-box");
 gift.src = giftBox;
 
-function resetHeart() {
-  heart.classList.remove("active");
-}
+gift.addEventListener("click", () => {
+  const popSound = new Audio(pop);
+  popSound.volume = 0.3;
+  popSound.play();
+});
 
 gift.addEventListener("click", () => {
-  heart.classList.add("active");
+  heart.style.transform = "translateY(0px)";
+  heart.style.transition = "none";
+  heart.style.visibility = "hidden";
   setTimeout(() => {
-    resetHeart();
-  }, 1600);
+    heart.style.visibility = "visible";
+    heart.style.transform = "translateY(-1000px)";
+    heart.style.transition = "2s ease-in";
+  }, 100);
 });
