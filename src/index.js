da9a0361601a47e8baec5fec2a76e2d257daee6a
Isa <isaismail3000@gmail.com>
2025-05-25 09:48:19 +0300

    Set img srcs, add active to h1

--- src/index.js
+++ src/index.js
@@ -17,9 +17,19 @@ gift.src = giftBox;
 const img1 = document.querySelector(".img1");
 img1.src = darkHeart;
 const img2 = document.querySelector(".img2");
-img2.src = flower;
+img2.src = lightHeart;
+const gift2 = document.querySelector(".gift-box2");
+gift2.src = openGiftBox;
 const img3 = document.querySelector(".img3");
-img3.src = lightHeart;
+img3.src = bow;
+const img4 = document.querySelector(".img4");
+img4.src = bow;
+const img5 = document.querySelector(".img5");
+img5.src = cat;
+const img6 = document.querySelector(".img6");
+img6.src = flower;
+const img7 = document.querySelector(".img7");
+img7.src = cat;
 
 // Play sound and show heart on click
 gift.addEventListener("click", () => {
@@ -42,12 +52,14 @@ gift.addEventListener("click", () => {
 // open gift box
 function open() {
   const page = document.querySelector(".page");
+  const h1 = document.querySelector("h1");
   let count = 0;
 
   gift.addEventListener("click", () => {
     count++;
     if (count == 3) {
       page.classList.add("active");
+      h1.classList.add("active");
     }
   });
 }
